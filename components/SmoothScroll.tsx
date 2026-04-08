77515369ddf229ad69258465243ec8c5cdf818e9
Tamilselvan G <tamilselvan9675@gmail.com>
2026-04-08 16:50:36 +0530

    lenis packages change

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,13 +6,12 @@ import Lenis from 'lenis';
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
+      lerp: 0.1,
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
-      touchMultiplier: 2,
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
     });
 
     let rafId: number;
